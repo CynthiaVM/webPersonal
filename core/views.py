9db36ab5d6bad606cf3894adac359edbd92f766b
CynthiaVM <cynthiamouzo@hotmail.com>
2021-08-26 01:18:42 -0300

    template

--- core/views.py
+++ core/views.py
@@ -10,27 +10,19 @@ html_base = """
 """
 
 def home(request):
-    return HttpResponse(html_base + """
-        <h2>Bienvenidos</h2>
-        <p>Esto es la portada.</p>
-    """)
+    return render(request, "core/home.html")
 
 def about(request):
-    return HttpResponse(html_base + """
-        <h2>Acerca de</h2>
-        <p>Mi nombre es Cynthia y estoy probando Django!</p>
-    """)
+    return render(request, "core/about.html")
+
+def portfolio(request):
+    return render(request, "core/portfolio.html")
 
 def contact(request):
-    return HttpResponse(html_base + """
-        <h2>Contacto</h2>
-        <p>Aquí os dejo mi email y mis redes sociales:</p>
-        <ul>
-            <li><a href="mailto:cynthiamouzo@hotmail.com">Email</a></li>
-            <li><a href="https://github.com/CynthiaVM">Github</a></li>
-            <li><a href="https://youtube.com">Youtube</a></li>
-        </ul>
-    """)
+    return render(request, "core/contact.html") 
+
+
+
 #html_base= ""
 
 #def home(request):
