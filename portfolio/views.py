b1b6919c76b7b272d6c3abc5233c642ad5c1c4f6
CynthiaVM <cynthiamouzo@hotmail.com>
2021-09-09 19:06:23 -0300

    terminado

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,5 +3,5 @@ from .models import Project
 
 def portfolio(request):
     projects = Project.objects.all()
-    return render(request, "portfolio/portfolio.html", {'projects':projects})
+    return render(request, 'portfolio.html', {'projects':projects})
 
